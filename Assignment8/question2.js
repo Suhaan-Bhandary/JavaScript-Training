44348b7a653b5ec790920053b095851dee6f5835
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-02-20 12:33:30 +0530

    feat(assignment 8): solved question 2

---
+++ Assignment8/question2.js
@@ -0,0 +1,16 @@
+/*
+    2. Using promises - write a function that fetches data from an API endpoint (GET https://reqres.in/api/users ).
+    Log the data into the console once it is received
+*/
+
+const fetchData = () => {
+  fetch("https://reqres.in/api/users")
+    .then((response) => {
+      if (!response.ok) throw Error("Error");
+      return response.json();
+    })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+};
+
+fetchData();
