8f587e7e69a364c85dedd126d5f477aa636d3663
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-03-11 14:47:25 +0530

    fix: moved schema to helper

--- React/formik-task-form/src/components/TaskForm/TaskForm.tsx
+++ React/formik-task-form/src/components/TaskForm/TaskForm.tsx
@@ -1,29 +1,18 @@
 import { useFormik } from "formik";
-import * as Yup from "yup";
-import { TaskFormInitialValues } from "../../helpers/task";
+import {
+  taskFormInitialValues,
+  taskFormValidationSchema,
+} from "../../helpers/task";
 import styles from "./TaskForm.module.css";
 
 const TaskForm = () => {
   const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
     useFormik({
-      initialValues: TaskFormInitialValues,
+      initialValues: taskFormInitialValues,
       onSubmit: (values) => {
         console.log(values);
       },
-      validationSchema: Yup.object({
-        title: Yup.string()
-          .required("title is required")
-          .max(20, "title length should be 20 or less"),
-        description: Yup.string().required("description is required"),
-        assignee: Yup.string().required("assignee is required"),
-        dueDate: Yup.date()
-          .typeError("dueDate required")
-          .required("dueDate is required")
-          .min(
-            new Date(new Date().setHours(0, 0, 0, 0)),
-            "Due date cannot be less than current date",
-          ),
-      }),
+      validationSchema: taskFormValidationSchema,
     });
 
   return (
--- React/formik-task-form/src/helpers/task.ts
+++ React/formik-task-form/src/helpers/task.ts
@@ -1,6 +1,23 @@
-export const TaskFormInitialValues = {
+import * as Yup from "yup";
+
+export const taskFormInitialValues = {
   title: "",
   description: "",
   assignee: "",
   dueDate: "",
 };
+
+export const taskFormValidationSchema = Yup.object({
+  title: Yup.string()
+    .required("title is required")
+    .max(20, "title length should be 20 or less"),
+  description: Yup.string().required("description is required"),
+  assignee: Yup.string().required("assignee is required"),
+  dueDate: Yup.date()
+    .typeError("dueDate required")
+    .required("dueDate is required")
+    .min(
+      new Date(new Date().setHours(0, 0, 0, 0)),
+      "Due date cannot be less than current date",
+    ),
+});
